faec9aea458cb4e260e3b707c4024fe405d607d4
SGR007 <sagarpardhi.test@gmail.com>
2022-07-15 15:28:01 +0000

    testing-tut

---
+++ src/Counter.test.js
@@ -0,0 +1,44 @@
+import { render, fireEvent } from "@testing-library/react";
+import Counter from "./Counter";
+
+describe(Counter, () => {
+  it("counter displays correct initial count", () => {
+    const { getByTestId } = render(<Counter initialCount={0} />);
+    const countValue = Number(getByTestId("count").textContent);
+    expect(countValue).toEqual(0);
+  });
+
+  it("count should increment by 1 if the increment button is clicked", () => {
+    const { getByRole, getByTestId } = render(<Counter initialCount={0} />);
+    const incrementButton = getByRole("button", { name: "Increment" });
+    const countValue1 = Number(getByTestId("count").textContent);
+    expect(countValue1).toEqual(0);
+    fireEvent.click(incrementButton);
+    const countValue2 = Number(getByTestId("count").textContent);
+    expect(countValue2).toEqual(1);
+  });
+
+  it("count should decrement by 1 if the decrement button is clicked", () => {
+    const { getByRole, getByTestId } = render(<Counter initialCount={0} />);
+    const decrementButton = getByRole("button", { name: "Decrement" });
+    expect(Number(getByTestId("count").textContent)).toEqual(0);
+    fireEvent.click(decrementButton);
+    expect(Number(getByTestId("count").textContent)).toEqual(-1);
+  });
+
+  it("count should be 0 if the restart button is clicked", () => {
+    const { getByRole, getByTestId } = render(<Counter initialCount={5} />);
+    const restartButton = getByRole("button", { name: "Restart" });
+    expect(Number(getByTestId("count").textContent)).toEqual(5);
+    fireEvent.click(restartButton);
+    expect(Number(getByTestId("count").textContent)).toEqual(0);
+  });
+
+  it("count should invert signs if the switch signs button is clicked", () => {
+    const { getByRole, getByTestId } = render(<Counter initialCount={50} />);
+    const switchButton = getByRole("button", { name: "Switch" });
+    expect(Number(getByTestId("count").textContent)).toEqual(50);
+    fireEvent.click(switchButton);
+    expect(Number(getByTestId("count").textContent)).toEqual(-50);
+  });
+});
